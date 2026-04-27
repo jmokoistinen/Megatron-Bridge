02ce9b6914574723cb2a9bba7047974711e145b5
Mika Koistinen <jmokoistinen@gmail.com>
2026-04-27 09:24:42 +0000

    get model imported

--- run_training.sh
+++ run_training.sh
@@ -1,36 +1,32 @@
-#ls ../Meg_brid/
 
+#srun --account=amd-tw-verification --ntasks=1 --gres=gpu:mi325:1 --time=3:00:00 --mem=64G --pty bash
+ 
 cd /shared_silo/scratch/mika/experiments/Megatron-Bridge
 
 source ~/environment.sh
-export CONTAINER=/shared_silo/scratch/mika/containers/build/rocm_primus_v25.11_transformers-5.5.4_linear_FA2.sif
+export CONTAINER=/shared_silo/scratch/containers/build-rocm_primus_v25.11_transformers-5.5.4_linear_FA/rocm_primus_v25.11_transformers-5.5.4_linear_FA.sif
 
 export PYTHONPATH=$(pwd)/python-packages:$(pwd)/3rdparty/Megatron-LM:$(pwd)/src:$PYTHONPATH #
 
-apptainer exec $CONTAINER python convert.py
+#download model (might work without, using hf folder directly)
+#hf download Qwen/Qwen3.5-35B-A3B --local-dir /shared_silo/scratch/mika/models/hf_models/qwen3.5-35B-A3B
 
+apptainer shell $CONTAINER
 
+# python 
+# Force partial import to cache submodules before the __init__ fails
+try:
+    import modelopt.torch.quantization.utils
+except ImportError:
+    pass
 
-# apptainer shell $CONTAINER
+from megatron.bridge.models.conversion import AutoBridge
 
-
-# # Force partial import to cache submodules before the __init__ fails
-# try:
-#     import modelopt.torch.quantization.utils
-# except ImportError:
-#     pass
-
-# from megatron.bridge.models.conversion import AutoBridge
-
-# AutoBridge.import_ckpt(
-#     hf_model_id="/shared_silo/scratch/mika/models/hf_models/qwen3.5-0.8b",
-#     megatron_path="/shared_silo/scratch/mika/models/megatron_ckpt/qwen3.5-0.8b",
-#     tp=1, pp=1,
-# )
-
-
-
-
+AutoBridge.import_ckpt(
+    hf_model_id="/shared_silo/scratch/mika/models/hf_models/qwen3.5-0.8b",
+    megatron_path="/shared_silo/scratch/mika/models/megatron_ckpt/qwen3.5-0.8b",
+    tp=1, pp=1,
+)
 
 
 # AutoBridge.import_ckpt(
@@ -40,23 +36,6 @@ apptainer exec $CONTAINER python convert.py
 #     pp=1, 
 # )
 
+#tokenize 
 
-# #export CONTAINER=/shared_silo/scratch/containers/rocm_primus_v25.11_transformers-5.5.0.sif
-# #export CONTAINER=/shared_silo/scratch/containers/rocm_primus_v25.11_transformers-4.5.7_linear_FA.sif
-
-# #export PYTHONPATH=$(pwd)/3rdparty/Megatron-LM:$(pwd)/src:$PYTHONPATH #
-
-# #singularity exec -B $PWD $CONTAINER pip install --target python-packages onnx 
-# #singularity exec -B $PWD $CONTAINER pip install --target python-packages omegaconf pulp 
-# #singularity exec -B $PWD $CONTAINER pip install --target python-packages flash-linear-attention #--no-deps
-# #singularity exec -B $PWD $CONTAINER pip install --target python-packages transformers
-
-
-
-# #done at start.py
-# import sys, os
-# #sys.path.insert(0, '/shared_silo/scratch/mika/experiments/Megatron-Bridge/src')
-# #sys.path.insert(0, '/shared_silo/scratch/mika/experiments/Megatron-Bridge/3rdparty/Megatron-LM')
-# sys.path.insert(0, os.getcwd()+'/src')
-# sys.path.insert(0, os.getcwd()+'/3rdparty/Megatron-LM')
-# import sys
+#training
